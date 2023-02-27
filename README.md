8a16d8045b77d66ef9c0c3f7e199692957744054
James Clemer <clemer19@gmail.com>
2023-02-26 19:14:06 -0500

    First attempt at a plasma/KDE and WiFi NixOS image build

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # laptop-nixos
+
+Nix files to install NixOS with Plasma to a Framework 12-gen laptop... hopefully.
+
+Working off of [this blag](https://blog.worobetz.ca/hardware/linux/nixos/2021/11/14/nixos-on-the-framework-laptop.html).
+
+
---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+# as root
+
+NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/74e2faf5965a12e8fa5cff799b1b19c6cd26b0e3.tar.gz nix-shell -p nixos-generators --run "nixos-generate --format iso --configuration ./build.nix -o result"
